0adb4bdb9e9eaa54a20cfd87c2efd95afa92a1d1
Rozz <rbanquerigo@reekoh.com>
2015-10-16 15:08:55 +0800

    Updates

---
+++ test/storage.test.js
@@ -0,0 +1,59 @@
+/*
+ * Just a sample code to test the storage plugin.
+ * Kindly write your own unit tests for your own plugin.
+ */
+'use strict';
+
+var cp     = require('child_process'),
+	assert = require('assert'),
+	storage;
+
+describe('Storage', function () {
+	this.slow(5000);
+
+	after('terminate child process', function () {
+		storage.kill('SIGKILL');
+	});
+
+	describe('#spawn', function () {
+		it('should spawn a child process', function () {
+			assert.ok(storage = cp.fork(process.cwd()), 'Child process not spawned.');
+		});
+	});
+
+	describe('#handShake', function () {
+		it('should notify the parent process when ready within 5 seconds', function (done) {
+			this.timeout(5000);
+
+			storage.on('message', function (message) {
+				if (message.type === 'ready')
+					done();
+			});
+
+			storage.send({
+				type: 'ready',
+				data: {
+					options: {
+						config_field1: 'configfieldvalue',
+						config_field2: 25
+					}
+				}
+			}, function (error) {
+				assert.ifError(error);
+			});
+		});
+	});
+
+	describe('#data', function () {
+		it('should process the data', function (done) {
+			storage.send({
+				type: 'data',
+				data: {
+					key1: 'value1',
+					key2: 121,
+					key3: 40
+				}
+			}, done);
+		});
+	});
+});
