2c935990d7edd585f8c05c47a2fb2064031736a1
Rozz <rbanquerigo@reekoh.com>
2015-11-13 15:14:35 +0800

    MySQL Updates

--- test/mysql-storage.test.js
+++ test/mysql-storage.test.js
@@ -5,11 +5,25 @@ const HOST     = 'reekoh-staging.cg1corueo9zh.us-east-1.rds.amazonaws.com',
 	  USER     = 'reekoh',
 	  PASSWORD = 'rozzwalla',
 	  DATABASE = 'reekoh',
-	  TABLE    = 'reekoh_table';
+	  TABLE    = 'reekoh_table',
+	  _ID  = new Date().getTime();
 
 var cp     = require('child_process'),
 	assert = require('assert'),
+	should   = require('should'),
+	moment   = require('moment'),
 	storage;
+
+var record = {
+	_id: _ID,
+	co2: '11%',
+	temp: 23,
+	quality: 11.25,
+	reading_time: '2015-11-27T11:04:13.539Z',
+	metadata: '{"metadata_json": "reekoh metadata json"}',
+	random_data: 'abcdefg',
+	is_normal: true
+};
 
 describe('Storage', function () {
 	this.slow(8000);
@@ -51,9 +65,14 @@ describe('Storage', function () {
 						database: DATABASE,
 						table: TABLE,
 						fields: JSON.stringify({
-							string_type: {
-								source_field: 'name', data_type: 'String'
-							}
+							_id				   : {source_field:'_id', data_type: 'Integer'},
+							co2_field      	   : {source_field:'co2', data_type: 'String'},
+							temp_field     	   : {source_field:'temp', data_type: 'Integer'},
+							quality_field  	   : {source_field:'quality', data_type: 'Float'},
+							reading_time_field : {source_field:'reading_time', data_type: 'DateTime', format: 'YYYY-MM-DDTHH:mm:ss'},
+							metadata_field 	   : {source_field:'metadata', data_type: 'String'},
+							random_data_field  : {source_field:'random_data'},
+							is_normal_field    : {source_field:'is_normal', data_type: 'Boolean'}
 						})
 					}
 				}
@@ -67,10 +86,52 @@ describe('Storage', function () {
 		it('should process the data', function (done) {
 			storage.send({
 				type: 'data',
-				data: {
-					name: 'rozz'
-				}
+				data: record
 			}, done);
 		});
 	});
+
+	describe('#data', function () {
+		it('should have inserted the data', function (done) {
+			this.timeout(10000);
+
+			var mysql = require('mysql');
+
+			var connection = mysql.createConnection({
+				host: HOST,
+				port: PORT,
+				user: USER,
+				password: PASSWORD,
+				database: DATABASE
+			});
+
+			connection.connect(function (err) {
+				connection.query('SELECT * FROM ' + TABLE + ' WHERE _id = ' + _ID, function (error, result, fields) {
+
+					should.exist(result[0]);
+					var resp = result[0];
+
+					//cleanup for JSON stored string
+					var cleanMetadata = resp.metadata_field.replace(/\\"/g, '"');
+					var str  = JSON.stringify(record.metadata);
+					var str2 = JSON.stringify(cleanMetadata);
+
+
+					should.equal(record.co2, resp.co2_field, 'Data validation failed. Field: co2');
+					should.equal(record.temp, resp.temp_field, 'Data validation failed. Field: temp');
+					should.equal(record.quality, resp.quality_field, 'Data validation failed. Field: quality');
+					should.equal(record.random_data, resp.random_data_field, 'Data validation failed. Field: random_data');
+					should.equal(moment(record.reading_time).format('YYYY-MM-DD HH:mm:ss'),
+						moment(resp.reading_time_field).format('YYYY-MM-DD HH:mm:ss'),
+						'Data validation failed. Field: reading_time');
+					should.equal(str, str2, 'Data validation failed. Field: metadata');
+
+					done();
+				});
+			});
+
+
+		});
+	});
+
 });
